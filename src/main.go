35d1107345fa7da7c418651b301237d87dea8c08
andrescaro16 <julicaro2003@gmail.com>
2023-05-06 14:45:26 -0500

    Variables

---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Constants declaration
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Integer declaration
+	var altura int = 10
+	var base int
+	altura2 := 20
+	fmt.Println(altura, base, altura2)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+
+}
