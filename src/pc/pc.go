095f7aacbfdfa58686a2d84809357cbd59b830a6
andrescaro16 <julicaro2003@gmail.com>
2023-05-07 13:20:34 -0500

    Using stringers to personalize the struct output

--- src/pc/pc.go
+++ src/pc/pc.go
@@ -16,3 +16,7 @@ func (myPC *Pc) DuplicateRAM() {
 	myPC.Ram = myPC.Ram * 2
 
 }
+
+func (myPC Pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d GB Disco y es una %s", myPC.Ram, myPC.Disk, myPC.Brand)
+}
