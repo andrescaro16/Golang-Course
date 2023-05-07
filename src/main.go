2e61da17daa405ce5ad35c2ffbbecbfbd3d7a02a
andrescaro16 <julicaro2003@gmail.com>
2023-05-06 22:19:53 -0500

    Range for slices and palindrome function

--- src/main.go
+++ src/main.go
@@ -1,32 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	var textReverse string
+
+	// Convert text to lower
+	text = strings.ToLower(text)
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palindromo")
+	} else {
+		fmt.Println("No es un palíndromo")
+	}
+}
 
 func main() {
+	slice := []string{"hola", "que", "hace"}
 
-	// Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	for i := range slice {
+		fmt.Println(i)
+	}
 
-	// Slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Slice methods
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append a new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
-
+	isPalindromo("amor a roma")
 }
