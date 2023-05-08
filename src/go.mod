3e7e39e508abc7f760b4a3ff121cc5aa30e6cbb5
andrescaro16 <julicaro2003@gmail.com>
2023-05-07 22:09:08 -0500

    Editing/personalizing external packages. In this case echo

--- src/go.mod
+++ src/go.mod
@@ -2,8 +2,9 @@ module github.com/andrescaro16/Golang-Course
 
 go 1.20
 
+require github.com/labstack/echo v3.3.10+incompatible
+
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -15,3 +16,5 @@ require (
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 )
+
+replace github.com/labstack/echo => ../echo
--- src/main.go
+++ src/main.go
@@ -1,3 +1,10 @@
+// 1. git clone to the package, in this example case echo
+// 2. Edit/personalice the package
+// 3. Run this command to replace the package we were using for the edited version: go mod edit -replace github.com/labstack/echo=../echo
+// 4. If you want to delete the edited package you can use: go mod edit -dropreplace github.com/labstack/echo
+// 5. To package all that we are using: go mod vendor
+// 6. To clean the packages that we're not using: go mod tidy
+
 package main
 
 import (
