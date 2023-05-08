8937f1ac1f554cc207ab4e2c941097b5683aa219
andrescaro16 <julicaro2003@gmail.com>
2023-05-07 19:18:51 -0500

    Close, range and select in channels

--- src/main.go
+++ src/main.go
@@ -1,18 +1,37 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func interaction(channel chan string)  {
-	var text string
-	fmt.Scanln(&text)
-	channel <- text
+func message(text string, c chan string) {
+	c <- text
 }
 
-func main()  {
-	channel := make(chan string, 1)
-	go interaction(channel)
-	message := <- channel
-	fmt.Println(message)
+func main() {
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
+
+	fmt.Println(len(c), cap(c))
+
+	// Close (To close the channel)
+	close(c)
+
+	// Range (To see the elements in the channel)
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 }
